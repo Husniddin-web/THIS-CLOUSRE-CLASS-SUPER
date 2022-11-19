5c10f0415deec23c3e96a14fed41527fe0244c54
Husniddin-web <husniddinsalohiddinov@gmail.com>
2022-11-19 14:37:32 +0500

    Constructor function class

---
+++ idnex.js
@@ -0,0 +1,54 @@
+"use strict"
+
+
+// Student constructor
+
+
+
+// class Student{
+//   constructor(name,age,city,lesson,unversity){
+//     this.name=name,
+//     this.age=age,
+//     this.city=city,
+//     this.lesson=lesson,
+//     this.unversity=unversity
+//   }
+//   borthYear(){
+//     let date=new Date
+//     return date.getFullYear() - this.age
+//   }
+//   allInfo(){
+//     console.log(`My nameis ${this.name} and I live in${this.city}.I am ${this.age} years old and born in ${this.borthYear()} year.Now I am stdying about ${this.lesson} in ${this.unversity} unversity`);
+//   }
+// }
+
+
+
+// let Husniddin=new Student('Husniddin',18,'Andijon','IT','Najot-Talim')
+// Husniddin.allInfo();
+
+
+
+
+// //  class  Fruits{
+// //   constructor(name,color,type,whenCome,whereCome,sum,kg){
+// //     this.name=name,
+// //     this.color=color,
+// //     this.type=type,
+// //     this.whereCome=whereCome,
+// //     this.whenCome=whenCome
+// //     this.sum=sum
+// //   }
+// //   total(){
+// //     let kilo=+prompt(`Necha killogram ${this.name} olmoqchisiz`)
+// //       return   ` ${kilo}  kilogram ${this.name} ${kilo*this.sum}`
+// //   }
+// //   allInfo(){
+// //     console.log(` ${this.name} mevasi ${this.whenCome} da yetishtirilgan va ${this.color} rangda  ${this.whereCome} kun  oldin kelgan  ${this.expireDays}  ${this.name} mevasi ${this.type} ga kiradi. ${this.total()} 1 kg ${this.name} ${this.sum} sum` );
+// //   }
+// //  }
+
+
+
+//  let olma=new Fruits('olma','qizil','tropik','USA',10, 4000)
+//  olma.allInfo();
